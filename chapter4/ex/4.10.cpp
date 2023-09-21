caa30795c6c179ce292f30671c809ebba9afc777
hsun <qiuxunxun@yahoo.com>
2023-09-22 00:05:31 +0800

    refactor 4.1-4.18

--- chapter4/ex/4.10.cpp
+++ chapter4/ex/4.10.cpp
@@ -1,13 +1,17 @@
+/*
+  Exercise 4.10: Write the condition for a while loop that would read ints from
+  the standard input and stop when the value read is equal to 42.
+ */
+
 #include <iostream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
   int i;
-  while(cin >> i && i !=  42){
+  while (cin >> i && i != 42) {
     cout << i << " ";
   }
 
---
+++ chapter4/ex/4.13.cpp
@@ -0,0 +1,6 @@
+int main() {
+  int i;
+  double d;
+  //  d = i = 3.5;
+  i = d = 3.5;
+}
