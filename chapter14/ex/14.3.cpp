e2402c8b6f6670dfeaff2106a61429e2c8892b9f
hsun <qiuxunxun@yahoo.com>
2023-09-27 21:32:48 +0800

    refactor 14.3-14.5

---
+++ chapter14/ex/14.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::vector<std::string> svec1;
+  std::vector<std::string> svec2;
+
+  "cabble" == "stone";
+
+  svec1[0] == svec2[0];
+
+  svec1 == svec2;
+
+  svec1[0] == "stone";
+}
